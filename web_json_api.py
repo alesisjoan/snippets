7e71c5ed261719812e20efb3eaa2187345775887
Alesis Manzano <amanzano@hespanol.com.ar>
2018-04-05 10:04:15 -0300

    se devuelve 200 OK

--- web_json_api.py
+++ web_json_api.py
@@ -14,11 +14,8 @@ app = web.application(urls, globals())
 class add:
     def POST(self):
         i = web.data()
-        print "antes del yield "+str(datetime.now())
-        yield datetime.now()
-        print "despues del yield "+str(datetime.now())
         print repr(i)
-#        return '200'
+        return '200'
 
 
 if __name__ == "__main__":
